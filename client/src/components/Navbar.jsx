35e099f9b1eff99bac4bd5702f80f39d2fba8be8
Muxammadayub7 <muxammadayubgithub@gmail.com>
2026-04-08 23:46:56 +0500

    add

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { assets } from '../assets/assets.js'
 import { MenuIcon, SearchIcon, XIcon } from 'lucide-react'
+import { useClerk, UserButton, useUser } from '@clerk/react'
 
 const Navbar = () => {
 	const [isOpen, setIsOpen] = useState(false)
+	const { user } = useUser()
+	const { openSignIn } = useClerk()
 
 	return (
 		<div className='fixed top-0 left-0 w-full z-50 flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
@@ -69,9 +72,13 @@ const Navbar = () => {
 
 			<div className='flex items-center gap-8'>
 				<SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer' />
-				<button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>
-					Login
-				</button>
+          {
+            !user ? (
+              <button onClick={openSignIn} className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Login</button>
+            ) : (
+              <UserButton />
+            )
+          }
 			</div>
 
 			<MenuIcon
